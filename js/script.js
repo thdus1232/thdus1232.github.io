9c5e6f10f5a31195235753baf0f0159789cdb4ad
thdus1232 <152838105+thdus1232@users.noreply.github.com>
2024-04-07 23:41:36 +0900

    index

--- js/script.js
+++ js/script.js
@@ -77,35 +77,211 @@ let scrollTween = gsap.to(sections, {
 // 가로 끝
 
 // index
-let links = gsap.utils.toArray("#index ul li a");
+// let links = gsap.utils.toArray("#index ul li a");
 
-links.forEach(link => {
-    let element = document.querySelector(link.getAttribute("href")),
-    linkST = ScrollTrigger.create({
-        trigger: element,
-        start: "top top"
-    });
+// links.forEach(link => {
+//     let element = document.querySelector(link.getAttribute("href")),
+//     linkST = ScrollTrigger.create({
+//         trigger: element,
+//         start: "top top"
+//     });
 
-    ScrollTrigger.create({
-        trigger: element,
-        containerAnimation: scrollTween,
-        start: "top center",
-        end: "bottom right",
+//     ScrollTrigger.create({
+//         trigger: element,
+//         containerAnimation: scrollTween,
+//         start: "top center",
+//         end: "bottom right",
+//         onToggle: () => setActive(link)
+//     });
+
+//     link.addEventListener("click", e => {
+//         e.preventDefault();
+//         gsap.to(window, {duration: 1, scrollTo: linkST.start, overwrite: "auto"});
+//     })
+// });
+
+// function setActive(link) {
+//     links.forEach(el => el.classList.remove("active"));
+//     link.classList.add("active");
+// };
+
+// new index start
+gsap.to(".step:nth-child(2)>a>img", {
+    opacity:1,
+    duration:1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section1",
+        start: "top top",
+        end:"+=1000",
+        toggleActions: "play reset play reset",
         markers: false,
-        onToggle: () => setActive(link)
-    });
-
-    link.addEventListener("click", e => {
-        e.preventDefault();
-        gsap.to(window, {duration: 1, scrollTo: linkST.start, overwrite: "auto"});
-    })
+        id: "2"
+    }
+});
+gsap.to(".step:nth-child(3)>a>img", {
+    opacity:1,
+    duration:1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section1",
+        start: "+=1000",
+        end:"+=1000",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "3"
+    }
+});
+gsap.to(".step:nth-child(4)>a>img", {
+    opacity:1,
+    duration:1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section1",
+        start: "+=2000",
+        end:"+=1000",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "4"
+    }
 });
 
-function setActive(link) {
-    links.forEach(el => el.classList.remove("active"));
-    link.classList.add("active");
-};
+// pixel step
+gsap.to(".step:nth-child(5)>a>img", {
+    autoAlpha: 1,
+    duration: 1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section3",
+        containerAnimation: scrollTween,
+        start: "top top",
+        end: "center left",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "s3"
+    }
+});
 
+// poly step
+gsap.to(".step:nth-child(6)>a>img", {
+    autoAlpha:1,
+    duration: 1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: ".polygon",
+        containerAnimation: scrollTween,
+        start: "left center",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "poly"
+    }
+});
+// banner step
+gsap.to(".step:nth-child(7)>a>img", {
+    autoAlpha: 1,
+    duration: 1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section4",
+        containerAnimation: scrollTween,
+        start: "left center",
+        end: "right center",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "s4"
+    }
+});
+// login step
+gsap.to(".step:nth-child(8)>a>img", {
+    autoAlpha:1,
+    duration: 1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section5",
+        containerAnimation: scrollTween,
+        start: "left center",
+        end: "center center",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "s5"
+    }
+});
+// namecard step
+
+gsap.to(".step:nth-child(9)>a>img", {
+    autoAlpha:1,
+    duration: 1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section5",
+        containerAnimation: scrollTween,
+        start: "center center",
+        end: "right center",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "name"
+    }
+});
+
+// neon step
+gsap.to(".step:nth-child(10)>a>img", {
+    autoAlpha:1,
+    duration: 1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section6",
+        containerAnimation: scrollTween,
+        start: "left center",
+        end: "center center",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "s6"
+    }
+});
+// card step
+gsap.to(".step:nth-child(11)>a>img", {
+    autoAlpha:1,
+    duration: 1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section6",
+        containerAnimation: scrollTween,
+        start: "left 20%",
+        end: "center center",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "card"
+    }
+});
+
+gsap.to(".step:nth-child(12)>a>img", {
+    opacity:1,
+    duration:1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section7",
+        start: "-=900",
+        end:"+=900",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "6"
+    }
+});
+gsap.to(".step:nth-child(13)>a>img", {
+    opacity:1,
+    duration:1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section7",
+        start:"top top",
+        end:"bottom top",
+        // start: "-=100",
+        // end:"+=700",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "ku"
+    }
+});
 // section7,8
 const ani7 = gsap.timeline();
 ani7.from("#section7 ul", {x: -150, opacity:0, duration:1},0)
@@ -118,9 +294,23 @@ ani7.from("#section7 ul", {x: -150, opacity:0, duration:1},0)
        end: "+=300",
        scrub: true,
        pin: true,
-       markers: false,
        anticipatePin: 1
    });
+
+
+gsap.to(".step:nth-child(14)>a>img", {
+    opacity:1,
+    duration:1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section8",
+        start: "-=300",
+        end:"+=1000",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "fnc"
+    }
+});
 
 const ani8 = gsap.timeline();
 ani8.from("#section8 ul", {x: -150, opacity:0, duration:1},0)
@@ -133,11 +323,24 @@ ani8.from("#section8 ul", {x: -150, opacity:0, duration:1},0)
        end: "+=300",
        scrub: true,
        pin: true,
-       markers: false,
        anticipatePin: 1
    });
 
 // contact 
+gsap.to(".step:nth-child(15)>a>img", {
+    opacity:1,
+    duration:1,
+    ease: "elastic",
+    scrollTrigger: {
+        trigger: "#section9",
+        start: "top center",
+        end:"+=2500",
+        toggleActions: "play reset play reset",
+        markers: false,
+        id: "9"
+    }
+});
+
 const ani9 = gsap.timeline();
 ani9.from("#section9 .nameen", {x: innerWidth * 1,duration:0.5})
     .from("#section9>.am>div:nth-child(2)", {x: innerWidth * -1})
